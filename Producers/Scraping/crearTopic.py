617df00ffb90d2680e87d9a52cd9d2966e5a598a
JulioCesarSalgado <julio.cesarsm@hotmail.es>
2023-10-04 17:56:13 -0700

    Modificando la creacion del topic condicionandolo si ya existe

--- Producers/Scraping/crearTopic.py
+++ Producers/Scraping/crearTopic.py
@@ -1,16 +1,23 @@
 import os
 from kafka.admin import KafkaAdminClient, NewTopic
 
-
 kafka_host = os.getenv('KAFKA_HOST', 'localhost')
 
 # Crear una instancia de un cliente administrativo de Kafka
 admin_client = KafkaAdminClient(bootstrap_servers=f'{kafka_host}:9092')
 
-print("Creando topico confirmación.")
+# Nombre del tópico que quieres crear
+topic_name = 'productos'
 
-# Crear una nueva instancia de topic
-topic = NewTopic(name='productos', num_partitions=10, replication_factor=2)
+# Verificar si el tópico ya existe
+topics = admin_client.list_topics()
+if topic_name in topics:
+    print(f"El tópico {topic_name} ya existe.")
+else:
+    print("Creando tópico confirmación.")
 
-# Crear el topic
-admin_client.create_topics([topic])
+    # Crear una nueva instancia de topic
+    topic = NewTopic(name=topic_name, num_partitions=10, replication_factor=2)
+
+    # Crear el topic
+    admin_client.create_topics([topic])
