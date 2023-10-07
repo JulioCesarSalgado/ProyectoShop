e1be235535948640ca46f8f2071372cfbda9d112
JulioCesarSalgado <julio.cesarsm@hotmail.es>
2023-10-06 18:20:53 -0700

    Solucionando el hard coding de los archivos scripts de inicio (start-script.sh)

--- Consumers/SaveDB/start-script.sh
+++ Consumers/SaveDB/start-script.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Cargar las variables de entorno
-source ../../.env
-
-
-echo "Comprobando la disponibilidad del broker-1..."
+echo "Comprobando la disponibilidad del broker-1 con la IP $KAFKA_HOST y el puerto $PORT_KAFKA_HOST..."
 
 # Esperar a que el broker-1 esté listo
-until nc -z $KAFKA_HOST 9092; do
-    echo "$(date) - esperando a que broker-1 esté listo..."
+until nc -z $KAFKA_HOST $PORT_KAFKA_HOST; do
+    echo "$(date) - esperando a que broker-1 con la IP $KAFKA_HOST y el puerto $PORT_KAFKA_HOST esté listo..."
     sleep 2
 done
 
-echo "El broker-1 está listo. Comprobando la disponibilidad del broker-2..."
+echo "El broker-1 está listo. Comprobando la disponibilidad del broker-2 con la IP $KAFKA_REMOTE y el puerto $PORT_KAFKA_REMOTE..."
 
 # Esperar a que el broker-2 esté listo
-until nc -z $KAFKA_REMOTE 9094; do
-    echo "$(date) - esperando a que broker-2 esté listo..."
+until nc -z $KAFKA_REMOTE $PORT_KAFKA_REMOTE; do
+    echo "$(date) - esperando a que broker-2 con la IP $KAFKA_REMOTE y el puerto $PORT_KAFKA_REMOTE esté listo..."
     sleep 2
 done
 
--- Producers/Scraping/start-script.sh
+++ Producers/Scraping/start-script.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# Cargar las variables de entorno
-source ../.env
-
-echo "Comprobando la disponibilidad del broker-1..."
+echo "Comprobando la disponibilidad del broker-1 con la IP $KAFKA_HOST y el puerto $PORT_KAFKA_HOST..."
 
 # Esperar a que el broker-1 esté listo
-until nc -z $KAFKA_HOST 9092; do
-    echo "$(date) - esperando a que broker-1 esté listo..."
+until nc -z -w5 $KAFKA_HOST $PORT_KAFKA_HOST; do
+    echo "$(date) - esperando a que broker-1 con la IP $KAFKA_HOST y el puerto $PORT_KAFKA_HOST esté listo..."
     sleep 2
 done
 
-echo "El broker-1 está listo. Comprobando la disponibilidad del broker-2..."
+echo "El broker-1 está listo. Comprobando la disponibilidad del broker-2 con la IP $KAFKA_REMOTE y el puerto $PORT_KAFKA_REMOTE..."
 
 # Esperar a que el broker-2 esté listo
-until nc -z $KAFKA_REMOTE 9094; do
-    echo "$(date) - esperando a que broker-2 esté listo..."
+until nc -z -w5 $KAFKA_REMOTE $PORT_KAFKA_REMOTE; do
+    echo "$(date) - esperando a que broker-2 con la IP $KAFKA_REMOTE y el puerto $PORT_KAFKA_REMOTE esté listo..."
     sleep 2
 done
 
 echo "El broker-2 está listo. Creando el topic..."
 
 # Iniciar tu aplicación
-sudo python3.10 /app/crearTopic.py && echo "Terminó de crear el topic. Usando el spider..." && sudo python3.10 /app/spiders/carso_spider.py
-
+python3.10 /app/crearTopic.py && echo "Terminó de crear el topic. Usando el spider..." && python3.10 /app/spiders/carso_spider.py
