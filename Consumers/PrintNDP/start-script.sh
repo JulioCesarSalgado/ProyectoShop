5ac033963bf120bc6bb3276b5c5770311e47d6b7
JulioCesarSalgado <julio.cesarsm@hotmail.es>
2023-10-09 16:04:42 -0700

    Modificando el script de comprobación de brokers del uso de ncat de linux al script Python comprobacion.py

--- Consumers/PrintNDP/start-script.sh
+++ Consumers/PrintNDP/start-script.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-echo "Comprobando la disponibilidad del broker-1 con la IP $KAFKA_HOST y el puerto $PORT_KAFKA_HOST..."
-
-# Esperar a que el broker-1 esté listo
-until nc -z $KAFKA_HOST $PORT_KAFKA_HOST; do
-    echo "$(date) - esperando a que broker-1 con la IP $KAFKA_HOST y el puerto $PORT_KAFKA_HOST esté listo..."
-    sleep 2
-done
-
-echo "El broker-1 está listo. Comprobando la disponibilidad del broker-2 con la IP $KAFKA_REMOTE y el puerto $PORT_KAFKA_REMOTE..."
-
-# Esperar a que el broker-2 esté listo
-until nc -z $KAFKA_REMOTE $PORT_KAFKA_REMOTE; do
-    echo "$(date) - esperando a que broker-2 con la IP $KAFKA_REMOTE y el puerto $PORT_KAFKA_REMOTE esté listo..."
-    sleep 2
-done
-
-echo "El broker-2 está listo. Creando consumidor..."
+#Comprobación de los brokers de los servidores Kafka
+python3.10 /app/comprobacion.py
 
 # Iniciar tu aplicación
 python3.10 /app/nomDescPrec.py
