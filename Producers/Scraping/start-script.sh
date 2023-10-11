cd8ad81baf8d63b9ad151a61e33d53a6edb54469
JulioCesarSalgado <julio.cesarsm@hotmail.es>
2023-10-11 13:04:06 -0700

    Modificando el archivo de comandos shell del spider para la comprobación de la base de datos MYSQL

--- Producers/Scraping/start-script.sh
+++ Producers/Scraping/start-script.sh
@@ -3,5 +3,11 @@
 #Comprobación de los brokers de los servidores Kafka
 python3.10 /app/comprobacion.py
 
+# Esperar a que la bse de datos esté lista
+until nc -z db_mysql 3306; do
+    echo "$(date) - esperando a que la base de datos esté lista..."
+    sleep 2
+done
+
 # Iniciar tu aplicación
 python3.10 /app/crearTopic.py && echo "Terminó de crear el topic. Usando el spider..." && python3.10 /app/spiders/carso_spider.py
