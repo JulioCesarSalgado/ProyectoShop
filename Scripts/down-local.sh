3c3df47e33d3f91c6dad0c159e44bd994b403cb5
JulioCesarSalgado <julio.cesarsm@hotmail.es>
2023-10-16 16:09:30 -0700

    Modificando el script para imprimir la ubicación de las carpetas en las que se encuentra el script y las carpetas/archivos que se eliminan

--- Scripts/down-local.sh
+++ Scripts/down-local.sh
@@ -5,7 +5,12 @@ docker compose -f docker-compose-local.yml down -v
 
 # Borrar volumenes
 for dir in "./Data_local/zookeeper_data" "./Data_local/kafka_data_1" "./Data_local/kafka_data_2" "./Data_local/mysql_data"; do
+  echo "Ubicación actual: $(pwd)"
   if [ -d "$dir" ] && [ "$(ls -A $dir)" ]; then
-    find "$dir" -type f -delete
+    echo "Cambiando a directorio: $dir"
+    cd "$dir"
+    echo "Ubicación después de cambiar: $(pwd)"
+    find . -depth -exec echo "Eliminando {} en $(pwd)" \; -exec rm -r {} \; || true
+    cd -
   fi
 done
--- Scripts/down-remote.sh
+++ Scripts/down-remote.sh
@@ -4,8 +4,13 @@
 docker compose -f docker-compose-remote.yml down -v
 
 # Borrar volumenes
-for dir in "./Data_remote/mysql_data" "./Data_remote/kafka_data_1" "./Data_remote/zookeeper_data"; do
+for dir in "./Data_remote/zookeeper_data" "./Data_remote/kafka_data_1" "./Data_remote/mysql_data"; do
+  echo "Ubicación actual: $(pwd)"
   if [ -d "$dir" ] && [ "$(ls -A $dir)" ]; then
-    find "$dir" -type f -delete
+    echo "Cambiando a directorio: $dir"
+    cd "$dir"
+    echo "Ubicación después de cambiar: $(pwd)"
+    find . -depth -exec echo "Eliminando {} en $(pwd)" \; -exec rm -r {} \; || true
+    cd -
   fi
 done
